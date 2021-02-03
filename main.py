63e477e894aacf4a761e880652d0f4719642d7c4
cfryhofer <77751743+cfryhofer@users.noreply.github.com>
2021-02-02 20:12:05 -0600

    zyLab 1.12

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 #Connor Fryhofer 1853826
-print('Birthday Calculator')
-print('Current day')
-m = int(input('Month: '))
-d = int(input('Year: '))
-y = int(input('Year: '))
-print('Birthday')
-m2 = int(input('Month: '))
-d2 = int(input('Day: '))
-y2 = int(input('Year: '))
-amtyears = y-y2-1
-if(m2<m):
-    amtyears+=1
-elif(m==m2):
-    if(d2<d):
-        amtyears+=1
-if(m==m2 and d==d2):
-    print('Happy Birthday')
-print('You are '+str(amtyears)+" years old.")
+user_num1 = int(input("Enter integer:\n"))
+print("You entered: " + str(user_num1))
+print(str(user_num1) + ' squared is ' + str(user_num1*user_num1))
+print('And ' + str(user_num1) + ' cubed is ' + str(user_num1**3) + ' !!')
+user_num2 = int(input('Enter another integer:\n'))
+print(str(user_num1) + ' + ' + str(user_num2) + ' is ' + str(user_num1+user_num2))
+print(str(user_num1) + ' * ' + str(user_num2) + ' is ' + str(user_num1*user_num2))
