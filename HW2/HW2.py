ee36ed24ae45bfe5e79b9a25ca4edc3ca3c5a850
cfryhofer <77751743+cfryhofer@users.noreply.github.com>
2021-03-02 23:13:45 -0600

    Add files via upload

---
+++ HW2/HW2.py
@@ -0,0 +1,34 @@
+list_Month = { "january": "1", "february": "2", "march": "3", "april": "4", "may": "5", "june": "6", "j": "7", "august": "8",
+               "september": "9", "october": "10", "november": "11", "december": "12"}
+
+input_file=open('hi-im-connor:\\HW2\\inputDates.txt','r')
+
+output_file=open('hi-im-connor:\\HW2\\parsedDates.txt','w')
+
+for each in input_file:
+    if each!="-1":
+        lis = each.split()
+        if len (lis) >=3:
+            Month=lis[0]
+            Day=lis[1]
+            Year=lis[2]
+
+            if Month.lower() in list_Month:
+                comma=Day[-1]
+                if comma==',':
+                    Day=Day[:len(Day)-1]
+                    Number_months=list_Month[Month.lower()]
+                    answer=Number_months+"/"+Day+"/"+Year
+
+                    output_file.write (answer)
+                    output_file.write("\n")
+
+
+
+
+output_file.close()
+input_file.close()
+
+
+
+
